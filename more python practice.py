bc7dec01efc9ff361b5869eda7c4782483f433db
toomeygm <114159795+toomeygm@users.noreply.github.com>
2022-11-14 22:24:03 +0100

    Attempting to finish task 2

---
+++ more python practice.py
@@ -0,0 +1,21 @@
+x = 100
+print (x)
+
+x %= 9
+print (x)
+
+print("U sure you\'re", num := int(input("How a=old are you?")), "years old?")
+print("Well, that\'s what I feel like")
+
+spam = "7"
+spam = spam + "0"
+eggs = int(spam) + 3
+print(float(eggs))
+
+y = 4.0
+print (type(y))
+
+cheeses = ['Cheddar', 'Edam', 'Gouda']
+
+for cheese in cheeses:
+    print (cheese)
---
+++ practice.py
@@ -0,0 +1,19 @@
+i = 1
+while i < 5:
+    print(i)
+    i += 1
+print("the end")
+
+adjs = ['shy', 'rare','endangered']
+birds = ['raven', 'shrike', 'redstart']
+
+for adj in adjs:
+    for bird in birds:
+        print(adj, bird)
+
+
+myName = input("enter name:")
+print("nice to meet you," + myName)
+
+print(len("nice to meet you"))
+
